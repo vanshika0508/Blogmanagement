5af03f62b38dc05fe42985e0bc097e5801235301
vanshika0508 <vermvans582000@gmail.com>
2025-11-07 12:15:53 -0500

    ICommentRepository created

---
+++ BlogManagement.Core/Interfaces/ICommentRepository.cs
@@ -0,0 +1,20 @@
+using BlogManagement.Core.Entities;
+
+namespace BlogManagement.Core.Interfaces
+{
+    public interface ICommentRepository
+    {
+        Task<IEnumerable<Comment>> GetAllAsync();
+        Task<IEnumerable<Comment>> GetByPOstIdAsync(int postId);
+        Task<Comment?> GetByIdAsync(int id);
+
+        Task<Comment> AddAsync(Comment comment);
+        Task UpdateAsync(Comment comment);
+
+        Task DeleteAsync(Comment comment);
+
+        Task<bool> ExistsAsync(int id);
+
+        Task SaveChangesAsync();
+    }
+}
