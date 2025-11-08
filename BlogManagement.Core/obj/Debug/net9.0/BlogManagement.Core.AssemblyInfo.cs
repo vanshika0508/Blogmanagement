64b5c0646411d25580fa61a20cb9b2857c1e5a96
vanshika0508 <vermvans582000@gmail.com>
2025-11-08 11:31:14 -0500

    DTOs folder created

--- BlogManagement.Core/obj/Debug/net9.0/BlogManagement.Core.AssemblyInfo.cs
+++ BlogManagement.Core/obj/Debug/net9.0/BlogManagement.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogManagement.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42f2eb805f582ff8716e72ffbdc35258ee0af0b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2f7be8f13c36a4e7d8c66f85c90ce4fe49a7696")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogManagement.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogManagement.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
