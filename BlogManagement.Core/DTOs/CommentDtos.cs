86c643c7dbdb0843fa2ce7c24c966023266530af
vanshika0508 <vermvans582000@gmail.com>
2025-11-08 11:56:37 -0500

    CommentCreate and update dtos created

--- BlogManagement.Core/DTOs/CommentDtos.cs
+++ BlogManagement.Core/DTOs/CommentDtos.cs
@@ -1,21 +1,62 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BlogManagement.Core.DTOs
 {
     public class CommentReadDto
     {
-         public int Id { get; set; }
+        public int Id { get; set; }
 
-        
+
         public int PostId { get; set; }
-       
-        public string Name { get; set; } ="";
-        
+
+        public string Name { get; set; } = "";
+
         public string Email { get; set; } = "";
 
         public string Content { get; set; } = "";
 
 
 
-        public DateTime CreatedDate { get; set; } 
+        public DateTime CreatedDate { get; set; }
 
     }
+
+    public class CommentCreateDto
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = "";
+        [Required]
+        [MaxLength(100)]
+
+        public string Email { get; set; } = "";
+        [Required]
+        [MaxLength(5000)]
+
+        public string Content { get; set; } = "";
+
+
+
+    }
+
+    public class CommentUpdateDto
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = "";
+        [Required]
+        [MaxLength(100)]
+
+        public string Email { get; set; } = "";
+        [Required]
+        [MaxLength(5000)]
+
+        public string Content { get; set; } = "";
+
+
+
+        
+    }
+    
+
 }
