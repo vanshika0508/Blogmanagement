3896cdbed92062ca105d3a9e7117658c462c3d21
vanshika0508 <vermvans582000@gmail.com>
2025-11-08 11:51:35 -0500

    comment read dto created

---
+++ BlogManagement.Core/DTOs/CommentDtos.cs
@@ -0,0 +1,21 @@
+namespace BlogManagement.Core.DTOs
+{
+    public class CommentReadDto
+    {
+         public int Id { get; set; }
+
+        
+        public int PostId { get; set; }
+       
+        public string Name { get; set; } ="";
+        
+        public string Email { get; set; } = "";
+
+        public string Content { get; set; } = "";
+
+
+
+        public DateTime CreatedDate { get; set; } 
+
+    }
+}
