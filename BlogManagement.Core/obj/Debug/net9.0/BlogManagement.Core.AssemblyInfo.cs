fd4a4772696b158f0224c4dd69bb1fe769cd0123
vanshika0508 <vermvans582000@gmail.com>
2025-11-17 20:16:15 -0500

    unknown

--- BlogManagement.Core/obj/Debug/net9.0/BlogManagement.Core.AssemblyInfo.cs
+++ BlogManagement.Core/obj/Debug/net9.0/BlogManagement.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogManagement.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ceaa67f85cedafb26491ae0c941757c70c270f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e493fec01b526c91396253e39c90c979ed997f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogManagement.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogManagement.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
